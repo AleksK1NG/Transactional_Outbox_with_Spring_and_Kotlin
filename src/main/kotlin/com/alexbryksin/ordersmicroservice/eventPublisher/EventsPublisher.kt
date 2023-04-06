aba2a504a7e3d3848a4ef44108e3159d4bf00345
Alexander <alexander.bryksin@yandex.ru>
2023-04-06 18:07:37 +0300

    feature: add outbox kafka consumer retry

--- src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
+++ src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
@@ -3,4 +3,5 @@ package com.alexbryksin.ordersmicroservice.eventPublisher
 interface EventsPublisher {
     suspend fun publish(topic: String?, data: Any)
     suspend fun publish(topic: String?, data: Any, headers: Map<String, ByteArray>)
+    suspend fun publishRetryRecord(topic: String?, data: ByteArray, headers: Map<String, ByteArray>)
 }
