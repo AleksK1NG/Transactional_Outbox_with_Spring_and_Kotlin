bdfb87ae6e79f84a57c33812f95ca38aac85e1c9
Alexander <alexander.bryksin@yandex.ru>
2023-04-09 16:06:55 +0300

    feature: add outbox kafka consumer methods

--- src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
+++ src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
@@ -2,6 +2,9 @@ package com.alexbryksin.ordersmicroservice.eventPublisher
 
 interface EventsPublisher {
     suspend fun publish(topic: String?, data: Any)
+    suspend fun publish(topic: String?, key: String, data: Any)
     suspend fun publish(topic: String?, data: Any, headers: Map<String, ByteArray>)
+    suspend fun publish(topic: String?, key: String, data: Any, headers: Map<String, ByteArray>)
     suspend fun publishRetryRecord(topic: String?, data: ByteArray, headers: Map<String, ByteArray>)
+    suspend fun publishRetryRecord(topic: String?, key: String, data: ByteArray, headers: Map<String, ByteArray>)
 }
