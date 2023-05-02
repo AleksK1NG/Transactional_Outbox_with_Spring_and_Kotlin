2e617ee442491ba06abd9321e63dd1b2125a751b
Alexander <alexander.bryksin@yandex.ru>
2023-05-02 18:34:39 +0300

    feature: add order kafka consumer retry improvements

--- src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
+++ src/main/kotlin/com/alexbryksin/ordersmicroservice/eventPublisher/EventsPublisher.kt
@@ -1,5 +1,7 @@
 package com.alexbryksin.ordersmicroservice.eventPublisher
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
 interface EventsPublisher {
     suspend fun publish(topic: String?, data: Any)
     suspend fun publish(topic: String?, key: String, data: Any)
@@ -7,4 +9,5 @@ interface EventsPublisher {
     suspend fun publish(topic: String?, key: String, data: Any, headers: Map<String, ByteArray>)
     suspend fun publishRetryRecord(topic: String?, data: ByteArray, headers: Map<String, ByteArray>)
     suspend fun publishRetryRecord(topic: String?, key: String, data: ByteArray, headers: Map<String, ByteArray>)
+    suspend fun publishRetryRecord(topic: String?, key: String, record: ConsumerRecord<String, ByteArray>)
 }
